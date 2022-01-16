9c80d9e858286939c91090a2671753dc84859cd1
kolberg-dev <74478295+kolberg-dev@users.noreply.github.com>
2022-01-16 11:24:20 +0100

    Initial commit

---
+++ src/lib/header/Header.svelte
@@ -0,0 +1,14 @@
+<header>
+  <nav>
+    hello
+  </nav>
+</header>
+
+<style>
+  header{
+    display: flex;
+    justify-content: space-between;
+    height: 2rem;
+    background-color: blueviolet;
+  }
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,22 @@
+<script>
+  let arr = new Array(400)
+</script>
+
+<div class="board">
+
+  {#each arr as a}
+  <div class="cell"></div>
+  {/each}
+</div>
+
+<style>
+  .board{
+    display: flex;
+    flex-wrap: wrap;
+  }
+  .cell{
+    border: solid 1px;
+    height: 20px;
+    aspect-ratio: 1;
+  }
+</style>
