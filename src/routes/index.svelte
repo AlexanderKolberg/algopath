37d27006273bb7e62c92e8fdd02e00d3943ddeca
kolberg-dev <74478295+kolberg-dev@users.noreply.github.com>
2022-01-17 08:49:27 +0100

    First draft of grid

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,22 +1,46 @@
-<script>
-  let arr = new Array(400)
+<script context="module">
+  import Viewport from 'svelte-viewport-info'
 </script>
-
-<div class="board">
-
-  {#each arr as a}
-  <div class="cell"></div>
-  {/each}
+<div 
+  on:mousedown={()=> (mousedown = true)} 
+  on:mouseup={() => (mousedown = false)}
+  
+ class="board">
+      {#each grid as row, i}
+        <div class="row">
+          {#each row as cell, k}
+            <div
+              class={`cell ${cell}`} 
+              on:mouseover={() => (mousedown ? grid[i][k] = "wall" : '')}
+              />
+          {/each}
+        </div>
+      {/each}
 </div>
+
+<script>
+  import { onMount } from 'svelte';
+  let mousedown = false
+  let rows = 0
+  let colums = 0
+  let grid = []
+  onMount(async () => {
+  colums = Math.floor(Viewport.Width/20)
+  rows = Math.floor(Viewport.Height/20)
+  grid = [...Array(colums)].map(() => [...Array(rows)])
+  })
+</script>
 
 <style>
   .board{
     display: flex;
-    flex-wrap: wrap;
   }
   .cell{
     border: solid 1px;
     height: 20px;
     aspect-ratio: 1;
+  }
+  .wall{
+    background: black;
   }
 </style>
