621c792d94780fe07b2633a10c9df9b0f2f623e4
kolberg-dev <74478295+kolberg-dev@users.noreply.github.com>
2022-02-06 18:57:58 +0100

    Added title tag

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,11 @@
 <script>
-  import Header from '$lib/header/Header.svelte';
-  import Board from '$lib/board/Board.svelte'
+	import Header from '$lib/header/Header.svelte';
+	import Board from '$lib/board/Board.svelte';
 </script>
+
+<svelte:head>
+	<title>AlgoPath</title>
+</svelte:head>
 
 <Header />
 <Board />
